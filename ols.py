51e7d79474e0548c35c8d302777a77ca6c16c574
rifat01-rahman <arifurrahmanrifat01@gmail.com>
2026-02-21 20:01:45 +0600

    OLS_example

--- ols.py
+++ ols.py
@@ -1,3 +1,23 @@
-df = " Hello World "
+import wooldridge as woo
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
 
-print (df.strip())
+# load dataset
+ceosal1 = woo.data("ceosal1")
+
+# OLS regression
+reg = smf.ols(formula="salary ~ roe", data=ceosal1)
+results = reg.fit()
+
+# scatterplot and fitted values
+plt.plot(ceosal1["roe"], ceosal1["salary"],
+         color="grey", marker="o", linestyle="")
+
+plt.plot(ceosal1["roe"], results.fittedvalues,
+         color="black", linestyle="-")
+
+plt.ylabel("salary")
+plt.xlabel("roe")
+
+plt.savefig("Example-2-3-3.pdf")
+plt.show()
